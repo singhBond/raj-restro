fec4bed013103062efcc79271d65b3c1a8494da0
_𝐵𝑜𝑛𝑑~007 <adityasingh05594@gmail.com>
2025-12-10 18:29:59 +0530

    Update WhatsApp contact number in Catalogue component

--- src/components/Catalogue.tsx
+++ src/components/Catalogue.tsx
@@ -400,7 +400,7 @@ const FastFoodCatalogue: React.FC = () => {
     message += `\n\nPlease confirm my order.`;
 
     window.open(
-      `https://wa.me/917369068632?text=${encodeURIComponent(message)}`,
+      `https://wa.me/916200656377?text=${encodeURIComponent(message)}`,
       "_blank"
     );
 
